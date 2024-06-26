dbd7d1944b0706b5fd0ec6bb245e51bde5c100d4
Praveen <praveen.km27@gmail.com>
2024-06-26 23:14:36 +0530

    Add files via upload

---
+++ day2/EdgeBrowser.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class EdgeBrowser {
+	public static void main(String[] args) {
+		
+		// Create an instance of the Browser class
+		Browser browser1 = new Browser();
+		
+		// Call the launchBrowser method with "Edge" as an argument and store the result in a String variable
+		String browserName = browser1.launchBrowser("Edge");
+		
+		System.out.println("The browser name is " + browserName);
+		
+		// Call the loadUrl method
+		browser1.loadUrl();
+		
+		}
+}
